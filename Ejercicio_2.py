ab06126771ef36f1d0b25c96c55d7d7c4a15c475
Mauricio Herraz <168691108+M4UR1C1O0@users.noreply.github.com>
2025-09-03 23:48:59 +0000

    Avance

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -7,3 +7,10 @@ datos = pd.read_csv('Datos_estudiantes.csv')
 
 # Creacioón del DataFrame
 df_alumnos = pd.DataFrame(datos)
+
+# Cuantos aprobaron con mayor a 4.0
+
+# Guardamos los aprobados en una nueva variable
+df_alumnos_aprobados = df_alumnos[df_alumnos['Promedio'] > 4.0]
+
+print(df_alumnos_aprobados)

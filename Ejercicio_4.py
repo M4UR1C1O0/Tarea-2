79d179c267d66cc4431bc0c15399a2380ceca5f6
ElFefoo <117328919+ElFefoo@users.noreply.github.com>
2025-09-04 00:50:58 +0000

    Realizacion ejercicio 4

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -7,3 +7,13 @@ datos = pd.read_csv('Datos_estudiantes.csv')
 
 # Creacioón del DataFrame
 df_alumnos = pd.DataFrame(datos)
+
+solo_notas = ['Nota1','Nota2','Nota3']
+
+notas= df_alumnos[solo_notas].values
+
+nota_baja_4 = np.any(notas < 4.0, axis=1)
+
+porcentaje = round(np.mean(nota_baja_4)*100,1)
+
+print("Porcentaje total de alumnos con al menos una nota bajo 4.0:",porcentaje,"%")

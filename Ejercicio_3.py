b5e1e0c5106fb72f55643c7d3ab65b69a31a7f58
Mauricio Herraz <168691108+M4UR1C1O0@users.noreply.github.com>
2025-09-04 00:46:10 +0000

    Avance ejercicio 3

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -7,3 +7,14 @@ datos = pd.read_csv('Datos_estudiantes.csv')
 
 # Creacioón del DataFrame
 df_alumnos = pd.DataFrame(datos)
+
+# Calcular la moda de las notas de los estudiantes
+Notas = ['Nota1', 'Nota2', 'Nota3']
+
+# Unimos todas las notas a una fila
+Notas_totales = pd.concat([df_alumnos[nota] for nota in Notas])
+
+# Sacamos la moda de donde tenemos todas las notas
+df_alumnos = Notas_totales.mode()[0]
+
+print('La moda de las notas de los estudiantes es:',df_alumnos)

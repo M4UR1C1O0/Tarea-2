679da4fddff2dd9174a4f8a96dd16385adee7039
ElFefoo <117328919+ElFefoo@users.noreply.github.com>
2025-09-04 01:13:24 +0000

    Realizacion ejercicio 5 y 6 y comentarios simples

--- Ejercicio_5.py
+++ Ejercicio_5.py
@@ -7,9 +7,9 @@ datos = pd.read_csv('Datos_estudiantes.csv')
 
 # Creacioón del DataFrame
 df_alumnos = pd.DataFrame(datos)
-
+# Calcular el promedio individual de cada estudiante usando axis para utilizar filas
 df_alumnos['Promedio'] = round(df_alumnos[['Nota1', 'Nota2', 'Nota3']].mean(axis=1),1)
-
+# Ordenar por la columna promedio de forma descendente utlizando sort_values y ademas reset_index para reiniciar los indices
 df_ordenado_descendente = df_alumnos.sort_values(by= 'Promedio',ascending=False).reset_index(drop=True)
 
 print("Listado Ordenado de forma Descendente de los promedios del curso",df_ordenado_descendente)
--- Ejercicio_6.py
+++ Ejercicio_6.py
@@ -7,3 +7,9 @@ datos = pd.read_csv('Datos_estudiantes.csv')
 
 # Creacioón del DataFrame
 df_alumnos = pd.DataFrame(datos)
+# Calcular el promedio individual de cada estudiante
+df_alumnos['Promedio'] = round(df_alumnos[['Nota1', 'Nota2', 'Nota3']].mean(axis=1),1)
+
+# Calcular el promedio total del curso, OJO redondeado a 1 decimal y es promedio 3.95 se aproxima a 4 
+promedio_total_curso = round(df_alumnos['Promedio'].mean(), 1)
+print("El promedio total del curso es:", promedio_total_curso,"%")
